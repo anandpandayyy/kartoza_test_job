7634ca661287138e917acc43a7cc27d81143254b
anand <anandpandayyy@gmail.com>
2022-10-18 13:14:55 +0530

    management command to add users in database

---
+++ project/app/management/commands/create_users.py
@@ -0,0 +1,18 @@
+from app.models import User
+from django.core.management.base import BaseCommand
+import json
+
+class Command(BaseCommand):
+    """
+    Creating user from json file
+    """
+
+    def handle(self, *args, **kwargs):
+        try:
+            with open('app/user_data.json') as json_file:
+                data = json.load(json_file)
+                User.objects.create_superuser(username=data['client'][0]['username'], email=data['client'][0]['email'], password=data['client'][0]['password'],lon=data['client'][0]['lon'],lat=data['client'][0]['lat'])
+                for new in data['client'][1:]:
+                    User.objects.create_user(username=new['username'], email=new['email'], password=new['password'],lon=new['lon'],lat=new['lat'])
+        except:
+            print("User's already created")
